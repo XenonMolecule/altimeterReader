57f1c7d5ab3322674d71f620d0481112a55d3b58
xenonmolecule <michael_ryan_2000@yahoo.com>
2015-12-28 15:33:50 +0000

    Made Audio Analyzer, preparing to specifically analyze altimeter readings.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // JavaScript File
+var microphone;
+var audio;
+var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+
 function hasGetUserMedia() {
   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia || navigator.msGetUserMedia);
@@ -19,12 +23,14 @@ if (hasGetUserMedia()) {
   console.log(navigator);
   if(navigator.getUserMedia){
     navigator.getUserMedia({audio: true}, function(stream) {
-      var microphone = context.createMediaStreamSource(stream);
+      audio = stream;
+      microphone = context.createMediaStreamSource(stream);
       var filter = context.createBiquadFilter();
   
       // microphone -> filter -> destination.
       microphone.connect(filter);
       filter.connect(context.destination);
+      setTimeout(initMp3Player, 100);
     }, function(e) { console.log('Ouch, get some ointment for that burn, because you were denied ', e)});
   } else {
     //failed test 2
@@ -35,4 +41,71 @@ if (hasGetUserMedia()) {
   //failed test 1
   alert("Sorry, your internet browser is not currently supported");
   console.log("Unsupported Browser")
+}
+/*
+// Create a new instance of an audio object and adjust some of its properties
+var audio = new Audio();
+audio.src = 'track1.mp3';
+audio.controls = true;
+audio.loop = true;
+audio.autoplay = true;
+// Establish all variables that your Analyser will use
+var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+// Initialize the MP3 player after the page loads all of its HTML into the window
+window.addEventListener("load", initMp3Player, false);
+function initMp3Player(){
+	document.getElementById('audio_box').appendChild(audio);
+	context = new webkitAudioContext(); // AudioContext object instance
+	analyser = context.createAnalyser(); // AnalyserNode method
+	canvas = document.getElementById('analyser_render');
+	ctx = canvas.getContext('2d');
+	// Re-route audio playback into the processing graph of the AudioContext
+	source = context.createMediaElementSource(audio); 
+	source.connect(analyser);
+	analyser.connect(context.destination);
+	frameLooper();
+}
+// frameLooper() animates any style of graphics you wish to the audio frequency
+// Looping at the default frame rate that the browser provides(approx. 60 FPS)
+function frameLooper(){
+	window.webkitRequestAnimationFrame(frameLooper);
+	fbc_array = new Uint8Array(analyser.frequencyBinCount);
+	analyser.getByteFrequencyData(fbc_array);
+	ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+	ctx.fillStyle = '#00CCFF'; // Color of the bars
+	bars = 100;
+	for (var i = 0; i < bars; i++) {
+		bar_x = i * 3;
+		bar_width = 2;
+		bar_height = -(fbc_array[i] / 2);
+		//  fillRect( x, y, width, height ) // Explanation of the parameters below
+		ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+	}
+}*/
+
+function initMp3Player(){
+	//document.getElementById('audio_box').appendChild(audio);
+	analyser = context.createAnalyser(); // AnalyserNode method
+	canvas = document.getElementById('analyser_render');
+	ctx = canvas.getContext('2d');
+	// Re-route audio playback into the processing graph of the AudioContext
+	microphone.connect(analyser);
+	analyser.connect(context.destination);
+	frameLooper();
+}
+
+function frameLooper(){
+	window.webkitRequestAnimationFrame(frameLooper);
+	fbc_array = new Uint8Array(analyser.frequencyBinCount);
+	analyser.getByteFrequencyData(fbc_array);
+	ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+	ctx.fillStyle = '#00CCFF'; // Color of the bars
+	bars = 100;
+	for (var i = 0; i < bars; i++) {
+		bar_x = i * 3;
+		bar_width = 2;
+		bar_height = -(fbc_array[i] / 2);
+		//  fillRect( x, y, width, height ) // Explanation of the parameters below
+		ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+  }
 }
