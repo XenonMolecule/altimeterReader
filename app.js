9d1ba216b0790242c3117b6735f2c75a3ae4651f
xenonmolecule <michael_ryan_2000@yahoo.com>
2015-12-28 06:49:55 +0000

    Added multibrowser support... I think

--- app.js
+++ app.js
@@ -12,14 +12,26 @@ if (hasGetUserMedia()) {
 
   var context = new AudioContext();
 
-navigator.webkitGetUserMedia({audio: true}, function(stream) {
-  var microphone = context.createMediaStreamSource(stream);
-  var filter = context.createBiquadFilter();
-
-  // microphone -> filter -> destination.
-  microphone.connect(filter);
-  filter.connect(context.destination);
-}, function(e) { console.log('Ouch, get some ointment for that burn, because you were denied ', e)});
+  navigator.getUserMedia  = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+  if(navigator.getUserMedia){
+    navigator.getUserMedia({audio: true}, function(stream) {
+      var microphone = context.createMediaStreamSource(stream);
+      var filter = context.createBiquadFilter();
+  
+      // microphone -> filter -> destination.
+      microphone.connect(filter);
+      filter.connect(context.destination);
+    }, function(e) { console.log('Ouch, get some ointment for that burn, because you were denied ', e)});
+  } else {
+    //failed test 2
+    alert("Sorry your internet browser is not currently supported");
+    console.log("Unsupported Browser Test 2");
+  }
+} else {
+  //failed test 1
+  alert("Sorry, your internet browser is not currently supported");
+  console.log("Unsupported Browser")
 }
-
-//function(e) { console.log('Ouch, get some ointment for that burn, because you were denied ', e)}
